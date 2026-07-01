5826ffb37c249d7e5383bdeeecd1c3f679819e43
Ali Al-sharqawi <ali.alsharqawi4@gmail.com>
2026-07-01 23:17:57 +0300

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix=strs[0];
+        for(const string&s:strs){
+            if(s == "")return "";
+            int i=0;
+            for(;i<s.size()&&i<prefix.size();++i){
+                if(s[i] != prefix[i]){
+                    break;
+                }
+            }
+            if (i==0)return "";
+            prefix = prefix.substr(0, i);
+        }
+        return prefix;
+
+    }
+};
